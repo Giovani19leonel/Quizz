e72033be07ac8c665c294c3745ae99a54fbb3593
Giovani19leonel <88912485+Giovani19leonel@users.noreply.github.com>
2021-09-09 00:31:39 -0300

    quizz

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace Giovani
             {
                 notas=notas+10;
                 lista[i]=resposta;
+                i++;
                 System.Console.WriteLine(resposta);
                 return(notas);
             }
@@ -28,6 +29,7 @@ namespace Giovani
             {
                 notas=notas+5;
                 lista[i]=resposta;
+                i++;
                 System.Console.WriteLine(resposta);
                 return(notas);
             }
@@ -35,6 +37,7 @@ namespace Giovani
             {
                 notas=notas+0;
                 lista[i]=resposta1;
+                i++;
                 System.Console.WriteLine(resposta1);
                 return(notas);
             }
@@ -75,67 +78,136 @@ namespace Giovani
                                 meth2();
                             } else meth3();
                         }
-                        Console.WriteLine(lista[0]);
-                        /*
 
                 Console.WriteLine("Qual o nome da funçao que pode chamar ela mesma?");
                     lista[2] = Convert.ToString(Console.ReadLine());
                         if (lista[2]=="recursiva")
                         {
                             meth();
-                        } else if(lista[2]=="recursiva") meth2();
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="recursiva"){
+                                meth2();
+                            } else meth3();
+                        }
 
                 System.Console.WriteLine("Qual o nome da estrutura de dados de variaveis de tipos diferentes?");
                     lista[3] =  Convert.ToString(Console.ReadLine());
                         if(lista[3]=="heterogenea")
                         {
                             meth();
-                        } else if(lista[3]=="heterogenea") meth2();
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="heterogenea")
+                            {
+                                meth2();
+                            } else meth3();
+                        }
 
                 System.Console.WriteLine("Qual o nome da estrutura de dados de variaveis de tipos iguais?");                                    
                     lista[4] = Convert.ToString(Console.ReadLine());
                         if(lista[4]=="homogenea")
                         {
                             meth();
-                        } else if(lista[4]=="homogenea") meth2();
+                        } 
+                        else
+                        {
+                            meth4(); 
+                            if(lista[i]=="homogenea")  
+                            {
+                                meth2();
+                            } else meth3();
+                        } 
 
                 System.Console.WriteLine("Qual o nome da incognita que não muda seu valor no algoritmo?");
                     lista[5] = Convert.ToString(Console.ReadLine());
                         if(lista[5]=="constante")
                         {
                             meth();
-                        } else if (lista[5]=="constante") meth2();
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="constante")
+                            {
+                                meth2();
+                            } else meth3();
+                        }
 
                 System.Console.WriteLine("Qual é o nome do tipo que é declarado instável?");
                     lista[6] = Convert.ToString(Console.ReadLine());
                         if(lista[6]=="variavel")
                         {
                             meth();
-                        } else if (lista[6]=="variavel") meth2();
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="variavel")
+                            {
+                                meth2();
+                            } else meth3();
+                        }
 
                 System.Console.WriteLine("O que é uma lista de variavel unidimensional?");
                     lista[7] = Convert.ToString(Console.ReadLine());
                         if(lista[7]=="vetor")
                         {
                             meth();
-                        } else if (lista[7]=="vetor") meth2();
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="vetor")
+                            {
+                                meth2();
+                            } else meth3();
+                        }
 
                 System.Console.WriteLine("Qual o nome da Sub-Rotina que não retorna valores");
                     lista[8] = Convert.ToString(Console.ReadLine());
                         if(lista[8]=="procedimentos")
                         {
                             meth();
-                        } else if (lista[8]=="procedimentos") meth2();
-
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="procedimentos")
+                            {
+                                meth2();
+                            } else meth3();
+                        }
                 System.Console.WriteLine("Qual é o nome generico do Dado que é passado de um lugar ao outro?");
                     lista[9] = Convert.ToString(Console.ReadLine());
                         if(lista[9]=="parametro")
                         {
                             meth();
-                        } else if (lista[9]=="parametro") meth2();
-
-            */
-                            
+                        } 
+                        else
+                        {
+                            meth4();
+                            if(lista[i]=="parametro")
+                            {
+                                meth2();
+                            } else meth3();
+                        }
+                        Console.WriteLine("---------------------------");
+                        Console.WriteLine("Notas total: {0}", notas);
+                        Console.WriteLine("1- {0}", lista[0]);
+                        Console.WriteLine("2- {0}", lista[1]);
+                        Console.WriteLine("3- {0}", lista[2]);
+                        Console.WriteLine("4- {0}", lista[3]);
+                        Console.WriteLine("5- {0}", lista[4]);
+                        Console.WriteLine("6- {0}", lista[5]);
+                        Console.WriteLine("7- {0}", lista[6]);
+                        Console.WriteLine("8- {0}", lista[7]);
+                        Console.WriteLine("9- {0}", lista[8]);
+                        Console.WriteLine("10- {0}", lista[9]);
         }
     }
 }
